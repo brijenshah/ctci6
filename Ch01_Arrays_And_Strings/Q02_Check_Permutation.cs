543b2cf915db38f365d75af1a0f6fbfa26be233b
brijenshah <brijen.shah@live.com>
2017-05-15 10:26:32 -0700

    Added Permutation logic

--- Ch01_Arrays_And_Strings/Q02_Check_Permutation.cs
+++ Ch01_Arrays_And_Strings/Q02_Check_Permutation.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Core;
 
 namespace Ch_01.Arrays_and_Strings
@@ -10,9 +6,40 @@ namespace Ch_01.Arrays_and_Strings
     [Question("ch01.q02", "Check Permutation")]
     public class Q02_Check_Permutation : IQuestion
     {
+        private bool CheckPermutation(string source, string target)
+        {
+            if (source.Length != target.Length) return false;
+
+            var sortedSource = new string(source.OrderBy(c => c).ToArray());
+            var sortedTarget = new string(target.OrderBy(c => c).ToArray());
+
+            return sortedSource.Equals(sortedTarget);
+        }
+
+        private bool CheckPermutation2(string source, string target)
+        {
+            if (source.Length != target.Length) return false;
+
+            int[] charMap = new int[256];
+
+            foreach (char c in source)
+            {
+                charMap[c]++;
+            }
+
+            foreach (char c in target)
+            {
+                if (charMap[c]-- < 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+
         public void Run()
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
